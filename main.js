46241f0e43ec278623215f951480ff40df408ccd
Leon Michalak <leonmichalak6@gmail.com>
2021-08-11 14:41:18 +0100

    Javascript bug fix

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 function toggleAccordion(num, totalAccordions) {
   // Num = the accordion number, totalAccordions = how many accordions in total
 
+  //   Only allows for one accordion to be active at once
   for (let i = 0; i < totalAccordions; i++) {
+    // Skip past the current accordion otherwise you won't be able to close it
+    if (i + 1 === num) {
+      continue;
+    }
+
     document.getElementById(`accordion-${i + 1}`).classList.remove('accordion-toggled');
   }
 
